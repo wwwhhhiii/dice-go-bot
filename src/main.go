ad4a2b32741c224afc816704b765eca56bef365d
Alexander <alexandr.s.trushnikov@gmail.com>
2022-12-04 17:02:29 +0500

    Removed coupled functionality

--- src/main.go
+++ src/main.go
@@ -8,15 +8,6 @@ import (
 	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func resolve_command(bot telegram.BotAPI, update telegram.Update) {
-	switch update.Message.Command() {
-	case "help":
-		controllers.Help(bot, update)
-	case "roll":
-		controllers.Roll_random_up_to(bot, update)
-	}
-}
-
 func main() {
 	settings := *core.NewSettings()
 	bot, updates := core.Startup(settings)
@@ -33,12 +24,13 @@ func main() {
 		}
 
 		commands_map := make(map[string]func(bot telegram.BotAPI, update telegram.Update))
+
+		// map commands to corresponding controllers
 		commands_map["help"] = controllers.Help
 		commands_map["roll"] = controllers.Roll_random_up_to
 
+		// Execute command
 		commands_map[update.Message.Command()](bot, update)
-
-		resolve_command(bot, update)
 	}
 
 	log.Print("Bot is down")
